fbf1904de141f0d1c03ac5eede2f808316636d7e
Fernando Arciga <ghero56@comunidad.unam.mx>
2021-11-25 18:24:50 -0600

    Code in Editor
    se añade el trabajo en bruto, aún sin crear ejecutable

---
+++ Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+  public float rotationSpeed = 1000;
+  
+  void Update() => transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+}
